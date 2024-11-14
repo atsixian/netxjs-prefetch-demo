4a19008e60600b36e1c2da2ff0bd2d7568320ae1
Sixian Li <43892874+atsixian@users.noreply.github.com>
2024-11-14 21:01:48 +0800

    layout loading

--- app/partial/modal/@m/(...)posts/[postId]/loading.js
+++
@@ -1,5 +0,0 @@
-export default async function Loading() {
-  console.log("/@m/posts/[postId]/loading");
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  return <p>postId Loading...</p>;
-}
--- app/partial/modal/@m/(...)posts/loading.js
+++ app/partial/modal/@m/(...)posts/loading.js
@@ -1,4 +1,5 @@
-export default function Loading() {
+export default async function Loading() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   console.log("@m/posts/loading");
   return <div>Posts Loading...</div>;
 }
