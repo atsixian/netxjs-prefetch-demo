4556f560e783c7523419276b78380c28a142d28f
Sixian Li <43892874+atsixian@users.noreply.github.com>
2024-11-14 14:00:35 +0800

    print before await

--- app/full/modal/@m/(...)posts/[postId]/page.js
+++ app/full/modal/@m/(...)posts/[postId]/page.js
@@ -1,5 +1,5 @@
 export default async function Page() {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
   console.log("/@m/page");
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   return <p>Intercepting route</p>;
 }
